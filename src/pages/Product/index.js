a7265c5c523fdb8a2a717fadec388e1f0fddaafc
MinXu <dotientai20031404@gmail.com>
2024-05-06 00:23:04 +0700

    update: product and list

--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -136,47 +136,142 @@ function Product() {
                                         <summary>
                                             <h3>Free Delivery and Returns</h3>
                                         </summary>
-                                        <div></div>
+                                        <div>
+                                            <div className={cx('prl1-sm')}>
+                                                <p>Your order of 5.000.000₫ or more gets free standard delivery.</p>
+                                                <br />
+                                                <ul className={cx('ncss-list-ul', 'mt4-sm', 'mb4-sm')}>
+                                                    <li className={cx('ncss-li')}>
+                                                        Standard delivered 4-5 Business Days
+                                                    </li>
+                                                </ul>
+                                                <br />
+                                                <p>
+                                                    Orders are processed and delivered Monday-Friday (excluding public
+                                                    holidays)
+                                                </p>
+                                                <br />
+                                                <p style={{ marginBottom: '40px' }}>
+                                                    Nike Members enjoy{' '}
+                                                    <u>
+                                                        <a href="https://www.nike.com/vn/help/a/returns-policy-gs">
+                                                            free returns
+                                                        </a>
+                                                    </u>
+                                                </p>
+                                            </div>
+                                        </div>
                                     </details>
                                     <details className={cx('css-1hbr3d8')}>
                                         <summary className={cx('css-rptnlm')}>
                                             <h3>Reviews (1)</h3>
                                             <div className={cx('css-n209rx')}>
-                                                <span className={cx('css-end85d')}>
-                                                    <span className={cx('mr1-sm')}>
-                                                        <span>
-                                                            <FontAwesomeIcon icon={faStar} />
-                                                        </span>
-                                                    </span>
-                                                    <span className={cx('mr1-sm')}>
-                                                        <span>
-                                                            <FontAwesomeIcon icon={faStar} />
-                                                        </span>
-                                                    </span>
-                                                    <span className={cx('mr1-sm')}>
-                                                        <span>
-                                                            <FontAwesomeIcon icon={faStar} />
-                                                        </span>
-                                                    </span>
-                                                    <span className={cx('mr1-sm')}>
-                                                        <span>
-                                                            <FontAwesomeIcon icon={faStar} />
-                                                        </span>
-                                                    </span>
-                                                    <span className={cx('mr1-sm')}>
-                                                        <span>
-                                                            <FontAwesomeIcon icon={faStar} />
-                                                        </span>
+                                                <span>
+                                                    <span className={cx('mr1-sm', 'd-sm-ib')}>
+                                                        <FontAwesomeIcon icon={faStar} />
+                                                    </span>{' '}
+                                                    <span className={cx('mr1-sm', 'd-sm-ib')}>
+                                                        <FontAwesomeIcon icon={faStar} />
+                                                    </span>{' '}
+                                                    <span className={cx('mr1-sm', 'd-sm-ib')}>
+                                                        <FontAwesomeIcon icon={faStar} />
+                                                    </span>{' '}
+                                                    <span className={cx('mr1-sm', 'd-sm-ib')}>
+                                                        <FontAwesomeIcon icon={faStar} />
                                                     </span>
                                                 </span>
                                             </div>
                                         </summary>
-                                        <div></div>
+                                        <div>
+                                            <div className={cx('css-1e0k2gt')}>
+                                                <div className={cx('reviews-component', 'mb5-sm')}>
+                                                    <div className={cx('product-review', 'mb10-sm')}>
+                                                        <div className={cx('star-rating', 'stars', 'd-sm-ib')}>
+                                                            <span>
+                                                                <span className={cx('mr1-sm', 'd-sm-ib')}>
+                                                                    <FontAwesomeIcon icon={faStar} />
+                                                                </span>
+                                                                <span className={cx('mr1-sm', 'd-sm-ib')}>
+                                                                    <FontAwesomeIcon icon={faStar} />
+                                                                </span>
+                                                                <span className={cx('mr1-sm', 'd-sm-ib')}>
+                                                                    <FontAwesomeIcon icon={faStar} />
+                                                                </span>
+                                                                <span className={cx('mr1-sm', 'd-sm-ib')}>
+                                                                    <FontAwesomeIcon icon={faStar} />
+                                                                </span>
+                                                            </span>
+                                                        </div>
+                                                        <p className={cx('d-sm-ib', 'pl4-sm')}>4 Starts</p>
+                                                        <p className={cx('pt2-sm', 'd-lg-b')}>
+                                                            <Button featured to="/">
+                                                                Write a Review
+                                                            </Button>
+                                                        </p>
+                                                    </div>
+                                                    <ul>
+                                                        <li className={cx('review', 'mb10-sm')}>
+                                                            <h4>Nike dunk lows</h4>
+                                                            <div className={cx('star-rating', 'stars', 'd-sm-ib')}>
+                                                                <span>
+                                                                    <span className={cx('mr1-sm', 'd-sm-ib')}>
+                                                                        <FontAwesomeIcon icon={faStar} />
+                                                                    </span>
+                                                                    <span className={cx('mr1-sm', 'd-sm-ib')}>
+                                                                        <FontAwesomeIcon icon={faStar} />
+                                                                    </span>
+                                                                    <span className={cx('mr1-sm', 'd-sm-ib')}>
+                                                                        <FontAwesomeIcon icon={faStar} />
+                                                                    </span>
+                                                                    <span className={cx('mr1-sm', 'd-sm-ib')}>
+                                                                        <FontAwesomeIcon icon={faStar} />
+                                                                    </span>
+                                                                </span>
+                                                            </div>
+                                                            <p
+                                                                className={cx(
+                                                                    'pl4-sm',
+                                                                    'd-sm-ib',
+                                                                    'text-color-secondary',
+                                                                    'responsive-body-3-2',
+                                                                )}
+                                                            >
+                                                                GiftA855881275 - 13 Apr 2024
+                                                            </p>
+                                                            <p
+                                                                className={cx(
+                                                                    'text-color-secondary',
+                                                                    'responsive-body-3-2',
+                                                                    'pb2-sm',
+                                                                    'u-align-left',
+                                                                )}
+                                                            ></p>
+                                                            <div>
+                                                                <p>
+                                                                    Awesome colour way, decent comfort, lots of lacing
+                                                                    options
+                                                                </p>
+                                                            </div>
+                                                        </li>
+                                                    </ul>
+                                                    <p className={cx('mt10-sm', 'mb10-sm')}>
+                                                        <Button featured to="/">
+                                                            <label className={cx('css-15juft3')}>More Reviews</label>
+                                                        </Button>
+                                                    </p>
+                                                </div>
+                                                <br />
+                                            </div>
+                                        </div>
                                     </details>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    <img
+                        src="https://static.nike.com/a/images/t_default/1419ed34-7762-420a-9f2a-2e2a65d5aba9/dunk-low-shoes-kKHp1z.png"
+                        alt=""
+                    />
                 </div>
             </div>
         </div>
