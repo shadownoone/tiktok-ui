18d3dd5eb783ba58c2d09db8ca11a9ea30a86730
MinXu <dotientai20031404@gmail.com>
2024-05-06 23:27:40 +0700

    update: header

--- src/components/Layout/components/Header/Header.js
+++ src/components/Layout/components/Header/Header.js
@@ -6,8 +6,6 @@ import { faHeart, faUser } from '@fortawesome/free-regular-svg-icons';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 import Search from '../Search';
-import MenuItem from './MenuItem';
-import MenuNotification from './MenuNotification';
 
 const cx = classNames.bind(styles);
 
@@ -24,47 +22,76 @@ function Header() {
                 </div>
                 <div className={cx('user-menu')}>
                     <ul className={cx('menu-list')}>
-                        <MenuItem title="Find a Store" className="custom-class" />
-                        <span>|</span>
-                        <MenuItem title="Help" className="custom-class">
+                        <li className={cx('list-item')}>
+                            <a href="/">Find a Store</a>
+                            <span>|</span>
+                        </li>
+
+                        <li className={cx('list-item')}>
+                            <a href="/"> Help</a> <span>|</span>
                             <ul className={cx('sub-menu')}>
                                 <h3>Help</h3>
-                                <MenuItem title="Order Status" />
-                                <MenuItem title="Dispatch and Delivery" />
-                                <MenuItem title="Returns" />
-                                <MenuItem title="Contact Us" />
-                                <MenuItem title="Privacy Policy" />
-                                <MenuItem title="Terms of Sale" />
-                                <MenuItem title="Terms of Use" />
-                                <MenuItem title="Send Us Feedback" />
+                                <li className={cx('sub-menu-item')}>
+                                    <a href="/">Order Status</a>
+                                </li>
+                                <li className={cx('sub-menu-item')}>
+                                    <a href="/">Dispatch and Delivery </a>
+                                </li>
+                                <li className={cx('sub-menu-item')}>
+                                    <a href="/">Returns </a>
+                                </li>
+                                <li className={cx('sub-menu-item')}>
+                                    <a href="/">Contact Us</a>{' '}
+                                </li>
+                                <li className={cx('sub-menu-item')}>
+                                    <a href="/">Privacy Policy</a>{' '}
+                                </li>
+                                <li className={cx('sub-menu-item')}>
+                                    <a href="/">Terms of Sale </a>
+                                </li>
+                                <li className={cx('sub-menu-item')}>
+                                    <a href="/">Terms of Use </a>
+                                </li>
+                                <li className={cx('sub-menu-item')}>
+                                    <a href="/">Send Us Feedback </a>
+                                </li>
                             </ul>
-                        </MenuItem>
+                        </li>
 
                         {/* <li className={cx('menu-item')}>
-                            <a href="/#">
-                                <span>Join Us</span>
-                                <span>|</span>
-                            </a>
-                        </li>
-                        <li className={cx('menu-item')}>
                             <a href="/#">
                                 <span>Sign In</span>
                             </a>
                         </li> */}
-                        <MenuItem title="Hi, Tai" className="custom-class">
+                        <li className={cx('list-item')}>
+                            <a href="/">Hi,Tai</a> {''}
                             <FontAwesomeIcon icon={faUser} />
                             <ul className={cx('sub-menu')}>
-                                <h3>Help</h3>
-                                <MenuItem title="Order Status" />
-                                <MenuItem title="Dispatch and Delivery" />
-                                <MenuItem title="Returns" />
-                                <MenuItem title="Contact Us" />
-                                <MenuItem title="Privacy Policy" />
-                                <MenuItem title="Terms of Sale" />
-                                <MenuItem title="Terms of Use" />
-                                <MenuItem title="Send Us Feedback" />
+                                <h3>Account</h3>
+                                <li className={cx('sub-menu-item')}>
+                                    <a href="/">Profile</a>
+                                </li>
+                                <li className={cx('sub-menu-item')}>
+                                    <a href="/">Orders</a>
+                                </li>
+                                <li className={cx('sub-menu-item')}>
+                                    <a href="/">Favourites</a>
+                                </li>
+                                <li className={cx('sub-menu-item')}>
+                                    <a href="/">Experiences</a>
+                                </li>
+
+                                <li className={cx('sub-menu-item')}>
+                                    <a href="/">Account Settings</a>
+                                </li>
+                                <li className={cx('sub-menu-item')}>
+                                    <a href="/">Terms of Use </a>
+                                </li>
+                                <li className={cx('sub-menu-item')}>
+                                    <a href="/">Log Out</a>
+                                </li>
                             </ul>
-                        </MenuItem>
+                        </li>
                     </ul>
                 </div>
             </div>
@@ -78,15 +105,72 @@ function Header() {
 
                 <div className={cx('action')}>
                     <ul className={cx('menu')}>
-                        <MenuItem title="New & Featured" />
-                        <MenuItem title="Men" hasSubMenu>
-                            <MenuNotification />
-                        </MenuItem>
+                        <li className={cx('menu-item')}>
+                            <a href="/">New & Featured</a>
+                        </li>
 
-                        <MenuItem title="Women" />
-                        <MenuItem title="Kids" />
-                        <MenuItem title="Customise" />
-                        <MenuItem title="SNKRS" />
+                        <li className={cx('menu-item')}>
+                            <a href="/">Men</a>
+                            <div className={cx('menu-notification')}>
+                                <div className={cx('sub-menu')}>
+                                    <h3>Shoes</h3>
+                                    <ul>
+                                        <li>
+                                            <a href="/">All Shoes</a>
+                                            <a href="/">All Shoes</a>
+                                            <a href="/">All Shoes</a>
+                                            <a href="/">All Shoes</a>
+                                            <a href="/">All Shoes</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className={cx('sub-menu')}>
+                                    <h3>Shoes</h3>
+                                    <ul>
+                                        <li>
+                                            <a href="/">All Shoes</a>
+                                            <a href="/">All Shoes</a>
+                                            <a href="/">All Shoes</a>
+                                            <a href="/">All Shoes</a>
+                                            <a href="/">All Shoes</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className={cx('sub-menu')}>
+                                    <h3>Shoes</h3>
+                                    <ul>
+                                        <li>
+                                            <a href="/">All Shoes</a>
+                                            <a href="/">All Shoes</a>
+                                            <a href="/">All Shoes</a>
+                                            <a href="/">All Shoes</a>
+                                            <a href="/">All Shoes</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className={cx('sub-menu')}>
+                                    <h3>Shoes</h3>
+                                    <ul>
+                                        <li>
+                                            <a href="/">All Shoes</a>
+                                            <a href="/">All Shoes</a>
+                                            <a href="/">All Shoes</a>
+                                            <a href="/">All Shoes</a>
+                                            <a href="/">All Shoes</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </li>
+                        <li className={cx('menu-item')}>
+                            <a href="/">Women</a>
+                        </li>
+                        <li className={cx('menu-item')}>
+                            <a href="/">Customise</a>
+                        </li>
+                        <li className={cx('menu-item')}>
+                            <a href="/">WoSNKRSmen</a>
+                        </li>
                     </ul>
                 </div>
                 <Search />
